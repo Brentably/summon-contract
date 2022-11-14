0a5ff5b4400c9602dbc171f9f52b7606a208eaa2
Brentably <brent@pabnet.com>
2022-11-14 12:47:20 -0600

    verify

--- contracts/Summon.sol
+++ contracts/Summon.sol
@@ -5,17 +5,33 @@ import './SummonUtils.sol';
 
 contract Summon is SummonUtils {
     address public immutable owner;
+    mapping(bytes => address) public storedTokens; // lender => (contract,tokenID) 
 
     constructor(address _owner) {
         owner = _owner;
+
     }
 
     
-    function depositToken external (
-        address tokenAddress
-        uint256 tokenID
-    ) {
+    function depositToken(
+        address tokenAddress,
+        uint256 tokenId
+    ) public returns(bool success, bytes memory data) {
+        bytes memory uniqToken = abi.encodePacked(tokenAddress, tokenId);
+        storedTokens[uniqToken] = msg.sender;
 
+        (success, data) = tokenAddress.delegatecall(abi.encodeWithSignature("transferFrom(address,address,uint256)",address(msg.sender),address(this),tokenId));
+        require(success, "failed");
+    }
+    
+
+    function withdrawToken(
+        address tokenAddress,
+        uint256 tokenId
+    ) public returns(bool success, bytes memory data) {
+        bytes memory uniqToken = abi.encodePacked(tokenAddress, tokenId);
+        require(storedTokens[uniqToken] == msg.sender, "withdrawer does not own token");
+        return tokenAddress.delegatecall(abi.encodeWithSignature("safeTransferFrom(address,address,uint256)",address(this),msg.sender,tokenId));
     }
 
    
